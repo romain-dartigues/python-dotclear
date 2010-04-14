42fc33e2788a70ef85024b69b00d93c4fdc4ddb9
Romain Dartigues <romain.dartigues@gmail.com>
2010-04-14 22:55:03 +0200

    - :class:`NodeMaz`: added binary arithmetic operators "+" and "|" ("add" and "bitwise or")

--- nodes.py
+++ nodes.py
@@ -1,4 +1,22 @@
-class MazNode:
+import copy
+
+
+
+
+__all__ = (
+	'MazNode',
+)
+
+
+INPLACE  = 0
+COPY     = 1
+DEEPCOPY = 2
+
+
+
+
+
+class MazNode(object):
 	'''
 	Usage:
 	>>> body = MazNode('body')
@@ -7,7 +25,7 @@ class MazNode:
 	>>> body.child+= MazNode('br')
 	>>> body.child+= MazNode('text')
 	>>> body.child&= MazNode('blockquote')
-	>>> body&= MazNode('foot')
+	>>> body|= MazNode('foot')
 	>>> body.next+= MazNode('text')
 	>>> print 'body (%d childs): %s' % (len(body), body)
 	body (2 childs): <body><p><strong /><br /><text /></p><blockquote /></body>
@@ -20,6 +38,7 @@ class MazNode:
 	  : <blockquote />
 	'''
 	__slots__ = ('name', 'parent', 'child', 'prev', 'next', 'attr')
+	__serial__ = 2010, 4,14
 	def __init__(self, name, **attr):
 		self.name   = name
 		self.parent = attr.pop('parent', None)
@@ -27,46 +46,71 @@ class MazNode:
 		self.next   = attr.pop('next', None) # next child
 		self.prev   = attr.pop('prev', None) # prev child
 		self.attr   = attr
-	def __iadd__(self, other):
-		'''Add `other` as a child'''
-		assert isinstance(other, MazNode)
-		if self.child:
-			child = self.child
+	@staticmethod
+	def __add(a, b, mode):
+		'''Base function for addition'''
+		assert isinstance(a, MazNode) and isinstance(b, MazNode)
+		if mode == COPY:
+			a = copy.copy(a)
+			b = copy.copy(b)
+		elif mode == DEEPCOPY:
+			a = copy.deepcopy(a)
+			b = copy.deepcopy(b)
+		if a.child:
+			child = a.child
 			while child.next:
 				child = child.next
-			child.next = other
-			other.prev = child
+			child.next = b
+			b.prev = child
+			b.parent = a
 		else:
-			self.child = other
-			other.parent = self
-		return self
-	def __iand__(self, other):
-		'''Add `other` as a brother'''
-		assert isinstance(other, MazNode)
-		if self.next:
-			next = self.next
+			a.child = b
+			b.parent = a
+		return a
+	def __iadd__(self, other):
+		'''Add `other` as a child, in-place'''
+		return self.__add(self, other, mode=INPLACE)
+	def __ladd__(self, other):
+		return self.__add(self, other, mode=DEEPCOPY)
+	def __radd__(self, other):
+		return self.__add(other, self, mode=DEEPCOPY)
+	def __add__(self, other):
+		'''Add `other` as a child and return a new object (deepcopy of self and other)'''
+		return self.__add(self, other, mode=DEEPCOPY)
+	@staticmethod
+	def __or(a, b, mode):
+		'''Base function for bitwise or'''
+		assert isinstance(a, MazNode) and isinstance(b, MazNode)
+		if mode == COPY:
+			a = copy.copy(a)
+			b = copy.copy(b)
+		elif mode == DEEPCOPY:
+			a = copy.deepcopy(a)
+			b = copy.deepcopy(b)
+		if a.next:
+			next = a.next
 			while next.next:
 				next = next
-			next.next = other
-			other.prev = next
+			next.next = b
+			b.prev = next
 		else:
-			self.next = other
-			other.prev = self
-		return self
-	def __add__(self, other):
-		assert isinstance(other, MazNode)
-		if self.child:
-			child = self.child
-			while child.next:
-				child = child.next
-			child.next = other
-			other.prev = child
-		else:
-			self.child = other
-			other.parent = self
-		return self
+			a.next = b
+			b.prev = a
+		return a
+	def __ior__(self, other):
+		'''Add `other` as a brother, in-place'''
+		return self.__or(self, other, INPLACE)
+	def __lor__(self, other):
+		return self.__or(self, other, DEEPCOPY)
+	def __ror__(self, other):
+		return self.__or(other, self, DEEPCOPY)
+	def __or__(self, other):
+		'''Add `other` as a brother and return a new object (deepcopy of self and other)'''
+		return self.__or(self, other, DEEPCOPY)
 	def __str__(self):
 		r = ['<', self.name]
+		# FIXME: don't rely on the slots
+		r+= [' %s=%r' %(attr, self.attr[attr]) for attr in self.attr if attr not in self.__slots__]
 		if self.child:
 			r+= ['>'] + [str(child) for child in self] + ['</', self.name, '>']
 		else:
@@ -102,16 +146,3 @@ class MazNode:
 		while child:
 			yield child
 			child = child.next
-	def copy(self):
-		return self.__class__(name=self.name, parent=self.parent, child=self.child, prev=self.prev, next=self.next, **self.attr)
-	def deepcopy(self):
-		copy = self.__class__(name=self.name)
-		for name in self.__slots__:
-			value = getattr(self, name, None)
-			if hasattr(value, 'deepcopy'):
-				value = value.deepcopy()
-			elif hasattr(value, '__iter__'):
-				value = value[:]
-			setattr(copy, name, value)
-		return copy
-
